af6c8f57b7fa92a70af39c61f0eb611c47915b4b
MaeveWheaton <teamheaven10@outlook.com>
2021-04-23 09:19:11 -0400

    Add project files.

---
+++ PlayGame/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace PlayGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.playButton = new System.Windows.Forms.Button();
+            this.countdownLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // playButton
+            // 
+            this.playButton.AutoSize = true;
+            this.playButton.BackColor = System.Drawing.Color.YellowGreen;
+            this.playButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.playButton.FlatAppearance.BorderSize = 5;
+            this.playButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.playButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.WhiteSmoke;
+            this.playButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.playButton.Font = new System.Drawing.Font("Open Sans Semibold", 14.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playButton.ForeColor = System.Drawing.Color.White;
+            this.playButton.Location = new System.Drawing.Point(239, 244);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(362, 141);
+            this.playButton.TabIndex = 0;
+            this.playButton.Text = "PLAY GAME";
+            this.playButton.UseVisualStyleBackColor = false;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // countdownLabel
+            // 
+            this.countdownLabel.BackColor = System.Drawing.Color.YellowGreen;
+            this.countdownLabel.Font = new System.Drawing.Font("Open Sans Semibold", 14.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countdownLabel.ForeColor = System.Drawing.Color.White;
+            this.countdownLabel.Location = new System.Drawing.Point(28, 413);
+            this.countdownLabel.Name = "countdownLabel";
+            this.countdownLabel.Size = new System.Drawing.Size(806, 110);
+            this.countdownLabel.TabIndex = 1;
+            this.countdownLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.YellowGreen;
+            this.ClientSize = new System.Drawing.Size(871, 746);
+            this.Controls.Add(this.countdownLabel);
+            this.Controls.Add(this.playButton);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Green Arcade Games";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Label countdownLabel;
+    }
+}
+
---
+++ PlayGame/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace PlayGame
+{  
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            //sounds setup
+            SoundPlayer playSound = new SoundPlayer(Properties.Resources.arcade_heal);
+            SoundPlayer countdownSound = new SoundPlayer(Properties.Resources.start_countdown);
+            SoundPlayer gameSound = new SoundPlayer(Properties.Resources.laserattack);
+
+            //play sound, remove start button 
+            playSound.Play();
+            Thread.Sleep(1500);
+            playButton.Visible = false;
+
+            //display changes and pause
+            Refresh();
+            Thread.Sleep(1000);
+
+            //countdown beep, begin countdown at 3
+            countdownSound.Play();
+            countdownLabel.Text = $"Game starts in: 3";
+            Refresh();            
+            
+            //1 sec pause           
+            Thread.Sleep(1000);
+
+            //countdown beep, count change to 2
+            countdownSound.Play();
+            countdownLabel.Text = $"Game starts in: 2";
+            Refresh();           
+
+            //1 sec pause           
+            Thread.Sleep(1000);
+
+            //countdown beep, count change to 1
+            countdownSound.Play();
+            countdownLabel.Text = $"Game starts in: 1";
+            Refresh();            
+
+            //1 sec pause           
+            Thread.Sleep(1000);
+
+            //play game sound 
+            gameSound.Play();
+
+            //change bg and font colours
+            this.BackColor = Color.Black;
+            countdownLabel.BackColor = Color.Black;
+            countdownLabel.ForeColor = Color.White;
+
+            //display "Go"
+            countdownLabel.Text = "GO";
+            Refresh();            
+        }
+    }
+}
